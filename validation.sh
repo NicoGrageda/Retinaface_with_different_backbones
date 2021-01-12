2afdde3251576555269e60e9a456f4f2bc055c37
NicoGrageda <55119202+NicoGrageda@users.noreply.github.com>
2021-01-12 10:54:26 -0300

    Update validation.sh

--- validation.sh
+++ validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for epoch in 210 220 230 240
+for epoch in 200 210 220 230 240
 do
-  python test_widerface.py --trained_model ./weights/efficientnet_b0_epoch_${epoch}.pth --network efficientnet_b0
+  python test_widerface.py --trained_model ./weights/mobilenetv3_large_100_epoch_${epoch}.pth --network mobilenetv3_large_100
   sleep 5
 done
