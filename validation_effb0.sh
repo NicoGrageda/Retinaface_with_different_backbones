9cc15942ef560ac09e7ed84d7fdc460e20d1f605
NicoGrageda <55119202+NicoGrageda@users.noreply.github.com>
2021-01-16 15:29:55 -0300

    Update validation_effb0.sh

--- validation_effb0.sh
+++ validation_effb0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for epoch in 190 200 205 215 225 235 245
 do
-  python test_widerface.py --trained_model ./weights/efficientnet_b0_epoch_${epoch}.pth --network efficientnet_b0
+  python test_widerface.py --trained_model ./weights/efficientnet_b0_optimizer_AdamP_epoch_${epoch}.pth --network efficientnet_b0
   sleep 5
 done
